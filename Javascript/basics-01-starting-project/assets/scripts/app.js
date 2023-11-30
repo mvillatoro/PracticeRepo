ef196e8c297839bc2b679be046cb792bb8bdccb7
Mario <mvilla2208@gmail.com>
2023-11-29 23:04:16 -0600

    added js course, added mew FE mentor challenge

---
+++ Javascript/basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,11 @@
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 + 1`;
+
+let errorMessage = "No pos guau.";
+
+outputResult(currentResult, calculationDescription);
---
+++ Javascript/basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+let userVar = 69;
+let result;
+
+result = userVar + 18;
+
+result = result - 12;
+result = result * 4;
+result = result / 12;
+
+alert(`userInput: ${userVar} `);
+alert(`result: ${result} `);
